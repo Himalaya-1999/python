cb867a872e677347b17cf772081a059db33b81bf
Himalaya sahu <63544465+Himalaya-1999@users.noreply.github.com>
2020-09-29 17:03:00 +0530

    Update web.py

--- Python_task/web.py
+++ Python_task/web.py
@@ -2,6 +2,7 @@
 
 import cgi
 import subprocess
+import webbrowser as wb
 
 print("content-type: text/html")
 print()
@@ -10,6 +11,8 @@ mydata = cgi.FieldStorage()
 
 var = mydata.getvalue("z")
 
+if("docker" in var):
+        wb.open("http://13.233.92.195/cgi-bin/Doc.py")
 
 output = subprocess.getoutput("sudo " + var)
 
